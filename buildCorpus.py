69e6e2a575433570222ccb1729bfa0519e6b5a87
Matthew Newsom <mbnewsom@calpoly.edu>
2012-10-14 16:29:02 -0700

    Added temp files sorted by rating.

--- buildCorpus.py
+++ buildCorpus.py
@@ -18,6 +18,14 @@ def buildCorpus(filepaths):
 def buildCorpus(filepaths):
    from nltk.corpus import PlaintextCorpusReader
 
+   corpus_root = 'C:\\Users\\Matthew\\AppData\\Local\Temp\\restaurantcorpus\\'
+   temp1 = open(corpus_root + "temp1.txt", 'w')
+   temp2 = open(corpus_root + "temp2.txt", 'w')
+   temp3 = open(corpus_root + "temp3.txt", 'w')
+   temp4 = open(corpus_root + "temp4.txt", 'w')
+   temp5 = open(corpus_root + "temp5.txt", 'w')
+   temps = [temp1, temp2, temp3, temp4, temp5]
+
    for path in filepaths:
       raw = open(path).read()
       
@@ -25,16 +33,22 @@ def buildCorpus(filepaths):
       service = re.findall(r'SERVICE: *(\d)', raw)
       venue = re.findall(r'VENUE: *(\d)', raw)
       rating = re.findall(r'RATING: *(\d)', raw)
+      ratings = [food, service, venue, rating]
 
       reviews = re.findall(r'WRITTEN REVIEW:\s*(.*\n)(.*\n)(.*\n)(.*\n)', raw)
+      i = 0
       for review in reviews:
-         print "Review:"
+         #print "Review:"
+         j = 0
          for para in review:
-            print para
+            temps[int(ratings[j][i])-1].write(para)
+            temps[int(ratings[j][i])-1].write('\n')
+            j += 1
+            #print para
+         i += 1
 
-   #corpus_root = '/tmp/'
-   #newcorpus = PlaintextCorpusReader(corpus_root, '.*')
-   #return newcorpus
+   newcorpus = PlaintextCorpusReader(corpus_root, '.*')
+   return newcorpus
    
 def main():
    buildCorpus(sys.argv[1:])
