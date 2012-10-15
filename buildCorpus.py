b0ebf061ed26aa6cce21238b3ebf8bb53c17634a
Andrew Guenther <guenther.andrew.j@gmail.com>
2012-10-14 21:42:15 -0700

    Refactoring

--- buildCorpus.py
+++ buildCorpus.py
@@ -18,13 +18,9 @@ def buildCorpus(filepaths):
 def buildCorpus(filepaths):
    from nltk.corpus import PlaintextCorpusReader
 
-   corpus_root = 'C:\\Users\\Matthew\\AppData\\Local\Temp\\restaurantcorpus\\'
-   temp1 = open(corpus_root + "temp1.txt", 'w')
-   temp2 = open(corpus_root + "temp2.txt", 'w')
-   temp3 = open(corpus_root + "temp3.txt", 'w')
-   temp4 = open(corpus_root + "temp4.txt", 'w')
-   temp5 = open(corpus_root + "temp5.txt", 'w')
-   temps = [temp1, temp2, temp3, temp4, temp5]
+   corpus_root = '/tmp/'
+
+   temps = [open(corpus_root + str(i) + ".txt", 'w') for i in range(1, 6)]
 
    for path in filepaths:
       raw = open(path).read()
@@ -36,16 +32,9 @@ def buildCorpus(filepaths):
       ratings = [food, service, venue, rating]
 
       reviews = re.findall(r'WRITTEN REVIEW:\s*(.*\n)(.*\n)(.*\n)(.*\n)', raw)
-      i = 0
-      for review in reviews:
-         #print "Review:"
-         j = 0
-         for para in review:
-            temps[int(ratings[j][i])-1].write(para)
-            temps[int(ratings[j][i])-1].write('\n')
-            j += 1
-            #print para
-         i += 1
+      for i, review in enumerate(reviews):
+         for j, para in enumerate(review):
+            temps[int(ratings[j][i])-1].write(para + '\n')
 
    newcorpus = PlaintextCorpusReader(corpus_root, '.*')
    return newcorpus
